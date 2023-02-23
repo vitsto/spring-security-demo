ecf2a3cabdd20566e6cae8a2b04338765cb0a210
vitsto <stolyarov.vitalij@list.ru>
2023-02-23 06:31:00 +0300

    initial commit

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/MySpringMvcDispatcherServerInitializer.java
@@ -0,0 +1,17 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServerInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{DemoAppConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Vitalij
+  Date: 23.02.2023
+  Time: 4:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>luv2code Company Home Page</title>
+</head>
+<body>
+<h2>luv2code Company Home Page</h2>
+<hr/>
+Welcome to the luv2code company home pPage
+</body>
+</html>
